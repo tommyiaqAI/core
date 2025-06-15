ef3a4fe5cbb7cc5f364ec4bad40ad103f96abc25
tommyiaqai <tommaso.iaquinta1991@gmail.com>
2025-06-15 18:07:04 +0200

    style mod

---
+++ site-config.js
@@ -0,0 +1,16 @@
+export default {
+  siteId: "site3",
+  storeName: "My Awesome Store",
+  contact: {
+    email: "info@myawesomestore.com",
+    phone: "+123456789"
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyAdFeVtKdb5oczNANmYPOeTkf2WbP_ruqg",
+    authDomain: "store-products-ed162.firebaseapp.com",
+    projectId: "store-products-ed162",
+    storageBucket: "store-products-ed162.firebasestorage.app",
+    messagingSenderId: "844599226880",
+    appId: "1:844599226880:web:241eeb3accf826a7005ee5"
+  }
+};
